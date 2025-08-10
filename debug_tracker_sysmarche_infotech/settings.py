b96250246d8903ff92416cd4fd1c7eb33779f2df
devendra-sysmarche <devendra.sysmarche@gmail.com>
2025-08-10 16:31:12 +0530

    Update db connection

--- debug_tracker_sysmarche_infotech/settings.py
+++ debug_tracker_sysmarche_infotech/settings.py
@@ -75,16 +75,16 @@ WSGI_APPLICATION = 'debug_tracker_sysmarche_infotech.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#     "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
